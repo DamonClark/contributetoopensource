65e2bf1833baa0963029d69f0b8e64b898f15e45
Damon Clark <dclark312@gmail.com>
2023-10-23 17:02:08 -0400

    updates for contribute to open source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-open-source-project';
+  labels: string = '';
+  language: string = '';
+  responseData: any;
+  url: any;
+
+  page = 1;
+  count = 0;
+  pageSize = 3;
+  pageSizes = [3, 6, 9];
+  
+  constructor(private appService: AppService) {}
+
+  groupedResults: { [repository_url: string]: any[] } = {};
+
+  
+  getGithubData() {
+    this.url = 'https://api.github.com/search/issues?q=label:' + this.labels + '+no:assignee\
+    +language:' + this.language +'+state:open&sort=created&order=desc&page=' + this.page + ''
+
+    console.log(this.page)
+
+    this.appService.getGitHubIssues(this.url).subscribe(data => {
+      this.responseData = data;
+      this.count = this.responseData.total_count;
+      console.log(this.responseData)
+      // Group search results by status
+      this.groupedResults = this.responseData.items.reduce((result: any, item: any) => {
+        const repository_url = item.repository_url;
+        if (!result[repository_url]) {
+          result[repository_url] = [];
+        }
+        result[repository_url].push(item);
+        return result;
+      }, {});
+    });;
+  };
+
+  // getRequestParams(page: number, pageSize: number): any {
+  //   // tslint:disable-next-line:prefer-const
+  //   let params: any = {};
+
+  //   if (page) {
+  //     params[`page`] = page - 1;
+  //   }
+
+  //   if (pageSize) {
+  //     params[`size`] = pageSize;
+  //   }
+
+  //   return params;
+  // }
+
+
+  handlePageChange(event: number): void {
+    this.page = event;
+    this.getGithubData();
+  }
+
+  handlePageSizeChange(event: any): void {
+    this.pageSize = event.target.value;
+    this.page = 1;
+    this.getGithubData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import this line
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NgxPaginationModule}  from 'ngx-pagination'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatPaginatorModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
