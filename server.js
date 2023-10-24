e1876e46763cea52f5b97ceb329dfdf45f3e1041
Damon Clark <dclark312@gmail.com>
2023-10-23 20:51:33 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/contributetoopensource'));
+app.use(express.static(__dirname + '/dist/contribute-to-opensource'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/contributetoopensource/index.html'));});
+'/dist/contribute-to-opensource/index.html'));});
 app.listen(process.env.PORT || 8080);
